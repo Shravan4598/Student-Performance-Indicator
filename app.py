699b2f3ef36c1a66cbc15b804710e39f6d1d4eec
Shravan4598 <shravankumarpandey825412@gmail.com>
2025-08-23 13:38:40 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+st.title("Student Score Prediction")
+
+# User Inputs
+gender = st.selectbox("Gender", ["male", "female"])
+race_ethnicity = st.selectbox("Race/Ethnicity", ["group A", "group B", "group C", "group D", "group E"])
+parental_level_of_education = st.selectbox("Parental Level of Education", [
+    "some high school", "high school", "some college", "associate's degree", "bachelor's degree", "master's degree"])
+lunch = st.selectbox("Lunch", ["standard", "free/reduced"])
+test_preparation_course = st.selectbox("Test Preparation Course", ["none", "completed"])
+reading_score = st.number_input("Reading Score", min_value=0, max_value=100, value=50)
+writing_score = st.number_input("Writing Score", min_value=0, max_value=100, value=50)
+
+# Predict Button
+if st.button("Predict"):
+    # Prepare input data
+    data = CustomData(
+        gender=gender,
+        race_ethnicity=race_ethnicity,
+        parental_level_of_education=parental_level_of_education,
+        lunch=lunch,
+        test_preparation_course=test_preparation_course,
+        reading_score=reading_score,
+        writing_score=writing_score
+    )
+    pred_df = data.get_data_as_data_frame()
+
+    # Prediction
+    predict_pipeline = PredictPipeline()
+    prediction = predict_pipeline.predict(pred_df)
+
+    st.success(f"Predicted Score: {prediction[0]}")
